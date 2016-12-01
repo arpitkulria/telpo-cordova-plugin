e7329acfbe1c55280a30c05cbff4cb2b7ff913a4
arpikulria <arpit.kulria@gmail.com>
2016-12-01 16:43:24 +0530

    Modified xml file

--- src/android/MagneticCard.java
+++ src/android/MagneticCard.java
@@ -52,7 +52,7 @@ public class MagneticCard extends CordovaPlugin {
 
     @Override
     protected void pluginInitialize() {
-        System.loadLibrary("/lib/armeabi");
+        //System.loadLibrary("/lib/armeabi");
     }
 
     @Override
