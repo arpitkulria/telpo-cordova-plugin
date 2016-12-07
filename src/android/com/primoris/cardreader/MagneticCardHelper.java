a9d79bce1f5a6ce7f2b689a764e7fec4da2d6297
arpikulria <arpit.kulria@gmail.com>
2016-12-07 12:17:09 +0530

    updated build.gradle

---
+++ src/android/com/primoris/cardreader/MagneticCardHelper.java
@@ -0,0 +1,76 @@
+//  Copyright (c) 2016 PayPal. All rights reserved.
+
+package com.primoris.cardreader;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import android.util.Log;
+import com.telpo.tps550.api.magnetic.MagneticCard;
+
+
+public class MagneticCardHelper extends CordovaPlugin {
+
+    @Override
+    protected void pluginInitialize() {
+//        System.loadLibrary("src/android/lib/native");
+//        System.loadLibrary("src/android/lib/armeabi");
+        System.out.println("\n\n\n In pluginInitialize function \n\n\n\n");
+    }
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        // TODO Auto-generated method stub
+        super.initialize(cordova, webView);
+        System.out.println("\n\n\n In initialize function \n\n\n\n");
+        Log.d("---------------------SAMPLE_ECHO---------------------", "--------------------- initializing ---------------------");
+    }
+
+    private CallbackContext callbackContext;
+    private Activity activity = null;
+    private static final int REQUEST_CARD_SCAN = 10;
+
+    @Override
+    public boolean execute(String action, JSONArray args,
+                           CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+        this.activity = this.cordova.getActivity();
+        boolean retValue = true;
+        if (action.equals("open")) {
+            System.out.println("\n\n\n In Action == open \n\n\n\n");
+            try {
+                this.open();
+            } catch (Exception ex) {
+                System.out.println("in teklpo exception");
+            }
+        } /*else if (action.equals("")) {
+            //
+        } else if (action.equals("")) {
+            //
+        }*/ else {
+            retValue = false;
+        }
+
+        return retValue;
+    }
+
+    public static void open() {
+        System.out.println("\n\n\n\n --------------------IN OPEN FUNCTION JAVA------------------------ \n\n\n\n\n ");
+        MagneticCard.open()
+    }
+}
--- www/cdv-plugin-card-io.js
+++ www/cdv-plugin-card-io.js
@@ -1,64 +1,12 @@
-/**
- * cdv-plugin-card-io.js
- *
- * Copyright 2013 PayPal Inc.
- * MIT licensed
- */
 
-/**
- * This class exposes card.io's card scanning functionality to JavaScript.
- *
- * @constructor
- */
-/*function CardIO() {
-}*/
-function MagneticCard() {
+function MagneticCardHelper() {
 
 }
-/*
 
-/!**
- * Scan a credit card with card.io.
- *
- *
- * @parameter options: an object; may be {}. Sample options object:
- *  {"collect_expiry": true, "collect_cvv": false, "collect_zip": false,
- *   "disable_manual_entry_buttons": false, "languageOrLocale": "en"}
- * Omit any key from options to get the default value. For more detail on
- * each of the options, look at CardIOPaymentViewController.h.
- *
- * @parameter onSuccess: a callback function that accepts a response object; response keys
- * include card_type, redacted_card_number, expiry_month, card_number, expiry_year,
- * and, if requested, cvv, and zip.
- *
- * @parameter onFailure: a zero argument callback function that will be called if the user
- * cancels card scanning.
- *!/
-CardIO.prototype.scan = function(options, onSuccess, onFailure) {
-  cordova.exec(onSuccess, onFailure, "CardIO", "scan", [options]);
-};
-
-/!**
- * Check whether card scanning is currently available. (May vary by
- * device, OS version, network connectivity, etc.)
- *
- * @parameter callback: a callback function accepting a boolean.
- *!/
-CardIO.prototype.canScan = function(callback) {
-  var failureCallback = function() {
-    console.log("Could not detect whether card.io card scanning is available.");
-    callback(false);
-  };
-  var wrappedSuccess = function(response) {
-    callback(response !== 0);
-  };
-  cordova.exec(wrappedSuccess, failureCallback, "CardIO", "canScan", []);
-};
-*/
 
 
 //--------------------------------------------------FIRST---------------------------------------------------------------
-MagneticCard.prototype.open = function() {
+MagneticCardHelper.prototype.open = function() {
   console.log("-----------In OPEN FUNCTION---------------------------");
     var failureCallback = function() {
         console.log("Problem while open");
@@ -67,66 +15,8 @@ MagneticCard.prototype.open = function() {
     var wrappedSuccess = function() {
         console.log("Open success");
     };
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCard", "open", []);
+  cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "open", []);
 };
 
 //----------------------------------------------------------------------------------------------------------------------
-
-
-MagneticCard.prototype.close = function(callback) {
-  console.log("-----------In CLOSE FUNCTION---------------------------");
-  var failureCallback = function() {
-    console.log("Could not detect whether MagneticCard close is available.");
-    callback(false);
-  };
-  var wrappedSuccess = function(response) {
-    callback(response !== 0);
-  };
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCard", "close", []);
-};
-
-MagneticCard.prototype.startReading = function(callback) {
-  console.log("-----------In startReading FUNCTION---------------------------");
-  var failureCallback = function() {
-    console.log("Could not detect whether MagneticCard startReading is available.");
-    callback(false);
-  };
-  var wrappedSuccess = function(response) {
-    callback(response !== 0);
-  };
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCard", "startReading");
-};
-
-
-MagneticCard.prototype.check = function(callback, timeout) {
-  console.log("-----------In check FUNCTION---------------------------");
-
-  var failureCallback = function() {
-    console.log("Could not detect whether MagneticCard check is available.");
-    callback(false);
-  };
-  var wrappedSuccess = function(response) {
-    callback(response !== 0);
-  };
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCard", "check", timeout);
-};
-/*
-/!**
- * Retrieve the version of the card.io library. Useful when contacting support.
- *
- * @parameter callback: a callback function accepting a string.
- *!/
-CardIO.prototype.version = function(callback) {
-  var failureCallback = function() {
-    console.log("Could not retrieve card.io library version");
-  };
-
-  cordova.exec(callback, failureCallback, "CardIO", "version", []);
-};
-
-
-/!**
- * Plugin setup boilerplate.
- *!/
-module.exports = new CardIO();*/
-module.exports = new MagneticCard();
+module.exports = new MagneticCardHelper();
