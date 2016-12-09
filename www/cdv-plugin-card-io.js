e9b6da1ef825c641714397e405560a94644c61b9
arpikulria <arpit.kulria@gmail.com>
2016-12-09 12:01:19 +0530

    Removed unwanted debuggings

--- www/cdv-plugin-card-io.js
+++ www/cdv-plugin-card-io.js
@@ -12,10 +12,30 @@ MagneticCardHelper.prototype.open = function() {
     var wrappedSuccess = function() {
         console.log("Open success");
     };
+
   cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "open", []);
 };
 
 
+MagneticCardHelper.prototype.startReading = function() {
+  console.log("-----------In startReading FUNCTION---------------------------");
+    var failureCallback = function() {
+        console.log("Problem while startReading");
+        return "Problem while startReading";
+        // reject("Problem whjile reading");
+    };
+
+    var wrappedSuccess = function(data) {
+        console.log( "Data from magnetic card >>>> " + data)
+        console.log("startReading success");
+        return data;
+        // resolve(data);
+    };
+
+  cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "startReading", []);
+};
+
+/*
 MagneticCardHelper.prototype.startReading = new Promise(function(resolve, reject) {
   console.log("-----------In startReading FUNCTION---------------------------");
     var failureCallback = function() {
@@ -33,5 +53,6 @@ MagneticCardHelper.prototype.startReading = new Promise(function(resolve, reject
 
   cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "startReading", []);
 });
+*/
 
 module.exports = new MagneticCardHelper();
