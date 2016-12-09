464b6171896f28a4a8911b2328895f7c8be9fc5c
arpikulria <arpit.kulria@gmail.com>
2016-12-09 15:10:30 +0530

    Updated js file

--- www/cdv-plugin-card-io.js
+++ www/cdv-plugin-card-io.js
@@ -4,7 +4,7 @@ function MagneticCardHelper() {
 }
 
 MagneticCardHelper.prototype.open = function() {
-  console.log("-----------In OPEN FUNCTION---------------------------");
+    console.log("-----------In OPEN FUNCTION---------------------------");
     var failureCallback = function() {
         console.log("Problem while open");
     };
@@ -13,7 +13,7 @@ MagneticCardHelper.prototype.open = function() {
         console.log("Open success");
     };
 
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "open", []);
+    cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "open", []);
 };
 
 
@@ -36,22 +36,24 @@ MagneticCardHelper.prototype.open = function() {
 // };
 
 
-MagneticCardHelper.prototype.startReading = function() {
-  console.log("-----------In startReading FUNCTION---------------------------");
+MagneticCardHelper.prototype.startReading = function(callback) {
+    console.log("-----------In startReading FUNCTION---------------------------");
     var failureCallback = function() {
         console.log("Problem while startReading");
-        return "Problem while startReading";
+        // return "Problem while startReading";
+        callback("Problem while startReading");
         // reject("Problem whjile reading");
     };
 
     var wrappedSuccess = function(data) {
         console.log( "Data from magnetic card >>>> ", data)
         console.log("startReading success");
-       return data;
-    //resolve(this.data);
+        callback(data)
+        // return data;
+        //resolve(this.data);
     };
     // var promise = new Promise()
-  cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "startReading", []);
+    cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "startReading", []);
 };
 
 
