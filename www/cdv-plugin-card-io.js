73eb581544253cb6db25078d49c3758287462a4f
arpikulria <arpit.kulria@gmail.com>
2016-12-09 15:52:24 +0530

    Updated js file

--- www/cdv-plugin-card-io.js
+++ www/cdv-plugin-card-io.js
@@ -38,22 +38,23 @@ MagneticCardHelper.prototype.open = function() {
 
 MagneticCardHelper.prototype.startReading = function(callback) {
     console.log("-----------In startReading FUNCTION---------------------------");
+
     var failureCallback = function() {
         console.log("Problem while startReading");
         // return "Problem while startReading";
-        callback("Problem while startReading");
+        //callback("Problem while startReading");
         // reject("Problem whjile reading");
     };
-
+/*
     var wrappedSuccess = function(data) {
-        console.log( "Data from magnetic card >>>> ", data)
+        console.log( "Data from magnetic card >>>> ", data);
         console.log("startReading success");
-        callback(data)
+        callback(data);
         // return data;
         //resolve(this.data);
-    };
+    };*/
     // var promise = new Promise()
-    cordova.exec(wrappedSuccess, failureCallback, "MagneticCardHelper", "startReading", []);
+    cordova.exec(callback, failureCallback, "MagneticCardHelper", "startReading", []);
 };
 
 
