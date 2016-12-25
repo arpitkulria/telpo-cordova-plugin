d43d97f3e9d96f1d0017e7861bcfb1d86253b06e
jyotsnakaran <jyotsna@knoldus.com>
2016-12-25 17:49:03 +0530

    Updated plugin for print functionality for logo MOB-24

--- src/android/com/primoris/cardreader/MagneticCardHelper.java
+++ src/android/com/primoris/cardreader/MagneticCardHelper.java
@@ -137,8 +137,7 @@ public class MagneticCardHelper extends CordovaPlugin {
             this.activity.runOnUiThread(new Runnable() {
                 public void run() {
                     try {
-                        System.out.println("Print agrs >>>>>>>>>>>>>>>>> "+args);
-                        int res = print(args.getString(0), args.getString(1));
+                        int res = print(args.getString(0), args.getString(1), args.getString(2));
                         callbackContext.success(res);
                     } catch (Exception ex) {
                         System.out.println("in telpo exception");
