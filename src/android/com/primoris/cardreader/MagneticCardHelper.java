d7c03f55c696f050b2d0c283e8d5e5e2712641ef
jyotsnakaran <jyotsna@knoldus.com>
2016-12-25 13:16:58 +0530

    Modified print method

--- src/android/com/primoris/cardreader/MagneticCardHelper.java
+++ src/android/com/primoris/cardreader/MagneticCardHelper.java
@@ -438,7 +438,6 @@ public class MagneticCardHelper extends CordovaPlugin {
 
             InputStream inputStream = null;
 
-            System.out.println("\n\n getAssets size >>>>>>>>>>>>>>>> "+this.activity.getApplicationContext().getAssets().list("").size());
             System.out.println("\n\n getAssets >>>>>>>>>>>>>>>> "+this.activity.getApplicationContext().getAssets().list("")[1]);
             inputStream = this.activity.getApplicationContext().getAssets().open("assets/primoris.png");
             Bitmap logoBitMap = BitmapFactory.decodeStream(inputStream);
