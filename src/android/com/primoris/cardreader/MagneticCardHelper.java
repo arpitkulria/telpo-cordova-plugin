0564b6fe9cccc1aef213b484ffb94759cb240f55
arpikulria <arpit.kulria@gmail.com>
2016-12-26 17:46:33 +0530

    Modified smarat card reading functionality

--- src/android/com/primoris/cardreader/MagneticCardHelper.java
+++ src/android/com/primoris/cardreader/MagneticCardHelper.java
@@ -178,18 +178,13 @@ public class MagneticCardHelper extends CordovaPlugin {
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(ReaderMonitor.ACTION_ICC_PRESENT);
-
-        System.out.println("<<<<<<<<<< Before register ??? >> " + chipData);
-
+        System.out.println("+++++++++++++++++++ Before register ??? >> " + chipData);
        Intent a =  this.activity.registerReceiver(mReceiver, filter);
-
-        String data = a.getStringExtra("key");
+        //String data = a.getStringExtra("key");
 
         Thread.sleep(5000);
-
-
-        System.out.println("<<<<<<<<<< After register ??? >> " + chipData);
-        System.out.println("<<<<<<<<<< data === " + data);
+        System.out.println("+++++++++++++++++++ After register ??? >> " + chipData);
+        System.out.println("+++++++++++++++++++ data === " + data);
 
         return chipData;
 
